25caf383ad646e3f07b31b058ce11365fd5e82e4
p22cgussen <p22cgussen@fairfieldprep.org>
2021-11-02 10:20:56 -0400

    start hw5-5-1

---
+++ hw5-5-1.py
@@ -0,0 +1,17 @@
+# Author CCG 11/2/21
+
+string = input('Enter a string: ')
+
+x = len(string)
+
+x1 = x // 2
+x2 = x1 + 1
+
+print(x1)
+print(x2)
+
+# half1 = string[0, int(x1)]
+# half2 = string[int(x2),]
+
+# print(half1)
+# print(half2)
