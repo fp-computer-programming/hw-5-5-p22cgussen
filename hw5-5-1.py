0642dc0fe8cd8d1b1578f00d32061398ccc98d7d
p22cgussen <p22cgussen@fairfieldprep.org>
2021-11-02 22:26:55 -0400

    complete hw5-5

--- hw5-5-1.py
+++ hw5-5-1.py
@@ -4,14 +4,7 @@ string = input('Enter a string: ')
 
 x = len(string)
 
-x1 = x // 2
-x2 = x1 + 1
+halfx = x // 2
 
-print(x1)
-print(x2)
-
-# half1 = string[0, int(x1)]
-# half2 = string[int(x2),]
-
-# print(half1)
-# print(half2)
+print(string[0:halfx])
+print(string[halfx:x])
